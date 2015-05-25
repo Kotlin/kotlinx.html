9db496fd476aaf6d2ce329c689718077820b6755
Sergey Mashkov <cy6erGn0m@gmail.com>
2015-05-25 23:03:12 +0300

    Tests for enums, forms, entities, loops

--- jvm/src/main/kotlin/main.kt
+++ jvm/src/main/kotlin/main.kt
@@ -11,62 +11,7 @@ import org.w3c.dom.Element
 import java.io.PrintStream
 import kotlin.dom.first
 
-fun <T> TagConsumer<T>.buildMe() = html { body { buildMe2() } }
-fun FlowContent.buildMe2() =
-				div(setOf("block", "deprecated")) {
-					a(href = "http://kotlinlang.org") {
-						target = ATarget.blank
-						attributes["custom"] = "custom"
-						+"test me"
-					}
-				}
-
 fun main(args : Array<String>) {
-	System.out.appendHTML().buildMe().println()
-	System.out.appendHTML().filter { if (it.tagName == "div") SKIP else PASS }.buildMe().println()
-	System.out.appendHTML().filter { if (it.tagName == "div") DROP else PASS }.buildMe().println()
-
-	println("deprecated:")
-	System.out.appendHTML().filter { if (it.tagName == "div" && "deprecated" in (it as DIV).classes) SKIP else PASS }.buildMe().append("\n")
-
-	System.out.appendHTML().html {
-		head {
-			title("test")
-			link { rel = LinkRel.stylesheet; href = "/styles.css" }
-		}
-		body {
-			form("/someurl") {
-				checkBoxInput(name = "cb1") {
-					+"var1"
-				}
-				input(type = InputType.checkBox, name = "cb2") {
-					disabled = true
-					+"var2"
-				}
-
-				buildMe2()
-
-				submitInput(content = "Go!")
-			}
-		}
-	}.println()
-
-	System.out.appendHTML().measureTime().html {
-		head {
-			title("Welcome page")
-		}
-		body {
-			div {
-				+"<<<special chars & entities goes here>>>"
-			}
-			div {
-			}
-		}
-	}.let {
-		it.first.println()
-		it.first.println("Generated in ${it.second} ms")
-	}
-
 	val document = document {
 		val html = createHTMLTree().html {
 			body {
--- jvm/src/test/kotlin/streaming.kt
+++ jvm/src/test/kotlin/streaming.kt
@@ -2,6 +2,7 @@ package html4k.tests
 
 import html4k.*
 import html4k.consumers.filter
+import html4k.consumers.measureTime
 import html4k.consumers.trace
 import html4k.stream.appendHTML
 import java.io.StringWriter
@@ -71,11 +72,13 @@ class TestStreaming {
     test fun `we should be able filter tags`() {
         val sw = StringWriter()
 
-        val result = sw.appendHTML(false).filter { when (it.tagName) {
-            "div" -> DROP
-            "pre" -> SKIP
-            else -> PASS
-        } }.html {
+        val result = sw.appendHTML(false).filter {
+            when (it.tagName) {
+                "div" -> DROP
+                "pre" -> SKIP
+                else -> PASS
+            }
+        }.html {
             body {
                 div {
                     div {
@@ -104,7 +107,7 @@ class TestStreaming {
 
         val sw2: StringWriter = result // note: this is to ensure result type is valid at compile time
 
-        assertEquals("<html><body><p>[pre.p]<a>[pre.p.pre.a]</a></p></body></html>", sw.toString())
+        assertEquals("<html><body><p>[pre.p]<a>[pre.p.pre.a]</a></p></body></html>", sw2.toString())
     }
 
     test fun `we should be able to handle many requests`() {
@@ -180,6 +183,81 @@ class TestStreaming {
     test fun `test tags order`() {
         assertEquals("<div><p><span></span></p></div>", StringBuilder().appendHTML(false).div { p { span {} } }.toString())
     }
+
+    test fun `test generated enum could be used`() {
+        assertEquals("<link rel=\"Stylesheet\" href=\"/path\"></link>", StringBuilder().appendHTML(false).link {
+            rel = LinkRel.stylesheet
+            href = "/path"
+        }.toString())
+    }
+
+    test fun `anchor with href syntax`() {
+        assertEquals("<a href=\"a.html\">text</a>", StringBuilder().appendHTML(false).a("a.html") {
+            +"text"
+        }.toString())
+    }
+
+    test fun `multiple content`() {
+        assertEquals("<span>AAAbbb...</span>", StringBuilder().appendHTML(false).span {
+            +"AAA"
+            +"bbb."
+            +".."
+        }.toString())
+    }
+
+    test fun `content with entity`() {
+        assertEquals("<span>before&amp;after</span>", StringBuilder().appendHTML(false).span {
+            +"before"
+            +Entities.amp
+            +"after"
+        }.toString())
+    }
+
+    test fun `test form with button`() {
+        assertEquals("<form action=\"/someurl\">" +
+        "<input type=\"checkbox\" name=\"cb1\">var1</input>" +
+        "<input type=\"checkbox\" name=\"cb2\" disabled=\"disabled\">var2</input>" +
+        "<input type=\"submit\">Go!</input>" +
+                "</form>",
+                StringBuilder().appendHTML(false).form("/someurl") {
+                    checkBoxInput(name = "cb1") {
+                        +"var1"
+                    }
+                    input(type = InputType.checkBox, name = "cb2") {
+                        disabled = true
+                        +"var2"
+                    }
+
+                    submitInput(content = "Go!")
+                }.toString())
+    }
+
+    test fun `test measure consumer with loop inside`() {
+        val count = 1000
+        val rs = StringBuilder(26 * (count + 1)).appendHTML(false).measureTime().div {
+            for (i in 1..count) {
+                div {
+                    p { +"node$i" }
+                }
+            }
+        }
+
+        assertTrue(rs.second > 0)
+        assertTrue(rs.second < count.toLong())
+
+        val expected = StringBuilder {
+            append("<div>")
+            for (i in 1..count) {
+                append("<div><p>")
+                append("node")
+                append(i)
+                append("</p></div>")
+            }
+            append("</div>")
+        }
+
+        assertEquals(expected.toString(), rs.first.toString())
+    }
 }
 
 fun <T> TagConsumer<T>.buildMe() = html { body { buildMe2() } }
@@ -192,7 +270,7 @@ fun FlowContent.buildMe2() =
             }
         }
 
-object NullAppendable: Appendable {
+object NullAppendable : Appendable {
     override fun append(csq: CharSequence?): Appendable = this
 
     override fun append(csq: CharSequence?, start: Int, end: Int): Appendable = this
