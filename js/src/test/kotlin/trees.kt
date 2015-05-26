546089b6192f1e1805c335d53d45a9b55c3af2bf
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2015-05-26 21:42:31 +0300

    Add test for event handling

--- js/src/test/kotlin/trees.kt
+++ js/src/test/kotlin/trees.kt
@@ -2,10 +2,16 @@ package html4k.tests
 
 import html4k.dom.append
 import html4k.js.div
+import html4k.js.onClickFunction
 import html4k.p
+import org.w3c.dom.events.Event
+import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.HTMLElement
 import org.w3c.dom.asList
+import kotlin.dom.asList
 import kotlin.browser.document
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 import org.junit.Test as test
 
@@ -24,4 +30,39 @@ class DomTreeImplTest {
         assertTrue(document.body!!.children.length > 0)
         assertEquals(node, document.body!!.children.asList().last())
     }
+
+    test fun appendSingleNode() {
+        val myDiv: HTMLDivElement = document.body!!.append.div {
+            p {
+                +"test"
+            }
+        }
+
+        assertEquals("DIV", myDiv.tagName)
+        assertEquals(document.body, myDiv.parentNode)
+        assertEquals("<div><p>test</p></div>", myDiv.outerHTML.replace("\\s+".toRegex(), ""))
+    }
+
+    test fun appendNodeWithEventHandler() {
+    	var clicked = false
+    	
+        document.body!!.append.div {
+            onClickFunction = {
+				clicked = true
+            }
+        }
+        
+        document.getElementsByTagName("div").asList().forEach {
+        	if (it is HTMLElement) {
+        		val clickHandler = it.onclick
+        		if (clickHandler != null) {
+        			clickHandler(uninitialized())
+        		}
+        	}
+        }
+        
+        assertTrue(clicked)
+    }
+    
+    private fun <T> uninitialized(): T = null as T 
 }
