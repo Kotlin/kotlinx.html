f0e5a431b0ad169960b1e07004acba19c1773628
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2016-02-20 17:40:34 +0300

    Add custom tag declaration test

---
+++ jvm/src/test/kotlin/custom-tag.kt
@@ -0,0 +1,49 @@
+package kotlinx.html.tests
+
+import kotlinx.html.*
+import kotlinx.html.stream.*
+import org.junit.*
+import kotlin.test.*
+
+class CustomTagTest {
+    @Test
+    fun testCustomTagInDiv() {
+        val html = buildString {
+            appendHTML(false).div {
+                custom {
+                    span { +"content" }
+                }
+            }
+        }
+
+        assertEquals("<div><custom><span>content</span></custom></div>", html)
+    }
+
+    @Test
+    fun testCustomTagRoot() {
+        val html =
+            buildString {
+                appendHTML(false).custom {
+                    span {
+                        +"content"
+                    }
+                }
+            }
+
+        assertEquals("<custom><span>content</span></custom>", html)
+    }
+}
+
+private class CUSTOM(consumer: TagConsumer<*>) :
+        HTMLTag("custom", consumer, emptyMap(),
+                inlineTag = true,
+                emptyTag = false), HtmlInlineTag {
+}
+
+private fun <T> TagConsumer<T>.custom(block: CUSTOM.() -> Unit = {}): T {
+    return CUSTOM(this).visitAndFinalize(this, block)
+}
+
+private fun DIV.custom(block: CUSTOM.() -> Unit = {}) {
+    CUSTOM(consumer).visit(block)
+}
