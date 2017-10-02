18ac1e4fbc861401e36a7147d567dbe3e1763be4
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-10-02 16:41:09 +0300

    Suppress multiplatform compilation error

--- shared/src/main/kotlin/Event.kt
+++ shared/src/main/kotlin/Event.kt
@@ -1,6 +1,6 @@
 package org.w3c.dom.events
 
-@Suppress("HEADER_WITHOUT_IMPLEMENTATION")
+@Suppress("HEADER_WITHOUT_IMPLEMENTATION", "NO_ACTUAL_FOR_EXPECT")
 header interface Event {
     fun stopPropagation()
     fun preventDefault()
