f927555e00d770da73f08a469ad58bd45b65980c
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2020-08-28 11:30:31 +0300

    Workaround undefined type on NodeJS (#159)
    Making these three functions inline eliminates element types usages and fixes the issue

--- src/jsMain/kotlin/compatibility-js.kt
+++ src/jsMain/kotlin/compatibility-js.kt
@@ -4,12 +4,12 @@ import kotlinx.html.*
 import org.w3c.dom.*
 
 @Deprecated("Use legend instead", ReplaceWith("legend(classes, block)"))
-fun TagConsumer<HTMLElement>.legEnd(classes : String? = null, block : LEGEND.() -> Unit = {}) : HTMLLegendElement = legend(classes, block)
+inline fun TagConsumer<HTMLElement>.legEnd(classes : String? = null, crossinline block : LEGEND.() -> Unit = {}) : HTMLLegendElement = legend(classes, block)
 
 @Deprecated("Use htmlObject instead", ReplaceWith("htmlObject(classes, block)", "kotlinx.html.js.htmlObject"))
-fun TagConsumer<HTMLElement>.object_(classes : String? = null, block : OBJECT.() -> Unit = {}) : HTMLElement =
+inline fun TagConsumer<HTMLElement>.object_(classes : String? = null, crossinline block : OBJECT.() -> Unit = {}) : HTMLElement =
         htmlObject(classes, block)
 
 @Deprecated("Use htmlVar instead", ReplaceWith("htmlVar(classes, block)", "kotlinx.html.js.htmlVar"))
-fun TagConsumer<HTMLElement>.var_(classes : String? = null, block : VAR.() -> Unit = {}) : HTMLElement =
+inline fun TagConsumer<HTMLElement>.var_(classes : String? = null, crossinline block : VAR.() -> Unit = {}) : HTMLElement =
         htmlVar(classes, block)
